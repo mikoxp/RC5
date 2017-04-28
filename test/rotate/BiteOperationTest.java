d22e5d5f10c81fba2761136c56656d0aec67e373
moles <olesmichalxp@gmail.com>
2017-04-28 13:39:43 +0200

    reversibility test

--- test/rotate/BiteOperationTest.java
+++ test/rotate/BiteOperationTest.java
@@ -6,6 +6,9 @@
 package rotate;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,24 +17,49 @@ import org.junit.Test;
  * @author moles
  */
 public class BiteOperationTest {
-    
-   private BiteOperation biteOperation=new BiteOperation();
-   
+
+    private BiteOperation biteOperation = new BiteOperation();
+
     @Test
-    public void testSomeMethod() {
-         BigInteger bigInteger=new BigInteger("abcd".getBytes());
-         int a= bigInteger.intValue();
-        int b=a;
-        int number=3;
-        int n=50;
-        for(int i=0;i<n;i++){
-            b=biteOperation.rotateLeft(b, number);
-            
+    public void reversibilityOfRotation_sameNumber_reversibility() {
+        BigInteger bigInteger = new BigInteger("abcd".getBytes());
+        int a = bigInteger.intValue();
+        int b = a;
+        int number = 3;
+        int n = 150;
+        for (int i = 0; i < n; i++) {
+            b = biteOperation.rotateLeft(b, number);
+
         }
-        for(int i=0;i<n;i++){
-            b=biteOperation.rotateRight(b, number);
+        for (int i = 0; i < n; i++) {
+            b = biteOperation.rotateRight(b, number);
         }
         Assert.assertEquals(a, b);
     }
-    
+
+    @Test
+    public void reversibilityOfRotation_randomNumber_reversibility() {
+        BigInteger bigInteger = new BigInteger("abcd".getBytes());
+        Random random = new Random();
+        List<Integer> integers = new ArrayList<>();
+        int a = bigInteger.intValue();
+        int b = a;
+        int number = 3;
+        int n = 150;
+        for (int i = 0; i < n; i++) {
+            number = random.nextInt() % 32;
+            number = Math.abs(number);
+            integers.add(number);
+        }
+        for (int i = 0; i < n; i++) {
+            number = integers.get(i);
+            b = biteOperation.rotateLeft(b, number);
+
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            number = integers.get(i);
+            b = biteOperation.rotateRight(b, number);
+        }
+        Assert.assertEquals(a, b);
+    }
 }
