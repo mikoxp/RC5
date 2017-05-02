7f5825eb6d406a12b44c547142216dffa3a908d3
moles <olesmichalxp@gmail.com>
2017-05-02 19:26:10 +0200

    generate info deleted

--- src/run/Runner.java
+++ src/run/Runner.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package run;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import key.BlumBlumShubKeyGenerator;
 import rc5.RC5Coder;
 import rc5.RC5Key;
 
