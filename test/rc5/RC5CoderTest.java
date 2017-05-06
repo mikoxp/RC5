7e92cae36184013cf0fb296a985019cb564dc5ab
moles <olesmichalxp@gmail.com>
2017-05-06 16:30:10 +0200

    coder test

---
+++ test/rc5/RC5CoderTest.java
@@ -0,0 +1,20 @@
+
+package rc5;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author moles
+ */
+public class RC5CoderTest {
+    
+    public RC5CoderTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
