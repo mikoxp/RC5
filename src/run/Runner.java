d94650ada319c3dae8158da0a129f76400e954bb
moles <olesmichalxp@gmail.com>
2017-05-19 14:33:20 +0200

    Block test

--- src/run/Runner.java
+++ src/run/Runner.java
@@ -3,9 +3,12 @@ package run;
 
 import exception.CoderException;
 import exception.KeyException;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import rc5.RC5Coder;
 import rc5.RC5Key;
+import time.tests.BlockTests;
 
 /**
  *
@@ -19,29 +22,25 @@ public class Runner {
      * @throws exception.KeyException
      */
     public static void main(String[] args) throws CoderException, KeyException {
-        int r = 255;
-        String text = "12345678asdfghjk1234abcdqwertyuq";
-        String key = "asdfghjklzxcvbnm";
-        RC5Key genratedKey=new RC5Key(r, 49);
-        RC5Key rc5key = new RC5Key(r, key.getBytes());
-        RC5Coder rC5Coder = new RC5Coder(r);
-         byte[] encrypt = rC5Coder.encrypt(text.getBytes(), genratedKey);
-        byte[] decrypt = rC5Coder.decrypt(encrypt, genratedKey);
-        System.out.println(text);
-        System.out.println(new String(encrypt));
-        System.out.println(new String(decrypt));
-        byte[] t=new byte[8];
-        t[0]=42;
-        t[1]=78;
-        t[2]=-109;
-        t[3]=115;
-        t[4]=84;
-        t[5]=12;
-        t[6]=21;
-        t[7]=-77;
-        byte[] encrypt1 = rC5Coder.encrypt(t, genratedKey);
-        byte[] d = rC5Coder.decrypt(encrypt1, genratedKey);
-        System.out.println(Arrays.equals(d, t));
+//        int r = 255;
+//        String text = "12345678asdfghjk1234abcdqwertyuq";
+//        String key = "asdfghjklzxcvbnm";
+//        RC5Key genratedKey=new RC5Key(r, 49);
+//        RC5Key rc5key = new RC5Key(r, key.getBytes());
+//        RC5Coder rC5Coder = new RC5Coder(r);
+//         byte[] encrypt = rC5Coder.encrypt(text.getBytes(), genratedKey);
+//        byte[] decrypt = rC5Coder.decrypt(encrypt, genratedKey);
+//        System.out.println(text);
+//        System.out.println(new String(encrypt));
+//        System.out.println(new String(decrypt));
+   
+        BlockTests blockTests=new BlockTests();
+        try {
+            blockTests.timeToEncryptTheNumberOfBlocks();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
 }
---
+++ src/time/tests/BlockTests.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package time.tests;
+
+import exception.CoderException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Date;
+import rc5.RC5Coder;
+import rc5.RC5Key;
+
+/**
+ *
+ * @author moles
+ */
+public class BlockTests {
+
+    private RC5Coder coder;
+    private RC5Key key;
+    private PrintWriter printWriter;
+    private final String path = "csv";
+
+    public BlockTests() {
+        try {
+            coder = new RC5Coder(255);
+            key = new RC5Key(255, 255);
+        } catch (CoderException ex) {
+
+        }
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    private long oneTimeEncrypt(int numberOFBlock) {
+        long result = 0;
+        long start = 0;
+        long end = 0;
+        byte[] data = new byte[numberOFBlock * 8];
+        start = System.currentTimeMillis();
+        try {
+            coder.encrypt(data, key);
+        } catch (CoderException ex) {
+            return -1;
+        }
+        end = System.currentTimeMillis();
+        result = end - start;
+        return result;
+    }
+
+    /**
+     *
+     * @throws FileNotFoundException
+     */
+    public void timeToEncryptTheNumberOfBlocks() throws FileNotFoundException {
+        Date d = new Date();
+        long time;
+        String fileName = path+"//number of block " + d.getDate() + "-" + d.getMonth() + " time " + d.getTime() + ".csv";
+        printWriter = new PrintWriter(new File(fileName));
+        int start=(int) Math.pow(10, 4);
+        int end=(int) Math.pow(10, 8);
+        int step=(int) Math.pow(10, 4);
+        for (int i = start; i <=end; i += step) {
+            time = oneTimeEncrypt(i);
+            printWriter.printf("%d;%d\n", i, time);
+        }
+        printWriter.close();
+        System.out.println("Finish Succesull: timeToEncryptTheNumberOfBlocks");
+    }
+
+}
